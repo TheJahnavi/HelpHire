5f70784e66c49b9ac3b247a92a6a4755de9ada65
Chandana Konduru <chandanasree000@gmail.com>
2025-09-23 18:44:03 -0500

    Update SpeedInsights component to track route changes in SPA

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,26 +2,23 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { useLocation } from "wouter";
 
 // Simple test to see if the React app is being rendered
 console.log("React app is starting...");
 
-// Add a simple test element to the DOM
-const testElement = document.createElement("div");
-testElement.id = "test-element";
-testElement.innerHTML = "<h1>React App is Running</h1>";
-testElement.style.position = "fixed";
-testElement.style.top = "0";
-testElement.style.left = "0";
-testElement.style.zIndex = "9999";
-testElement.style.backgroundColor = "red";
-testElement.style.color = "white";
-testElement.style.padding = "10px";
-document.body.appendChild(testElement);
+// Create a component that includes SpeedInsights with route tracking
+function AppWithSpeedInsights() {
+  const [location] = useLocation();
+  
+  return (
+    <>
+      <App />
+      <SpeedInsights route={location} />
+    </>
+  );
+}
 
 createRoot(document.getElementById("root")!).render(
-  <>
-    <App />
-    <SpeedInsights />
-  </>
+  <AppWithSpeedInsights />
 );
